2187793a4df8840ad5301f1ee2e87bedf63cee92
Oge <52808493+ogeitunu@users.noreply.github.com>
2023-09-10 16:23:12 +0100

    h

--- calculate.js
+++ calculate.js
@@ -1,25 +1,57 @@
-let displayValue = '';
-
-// append a value to the display
-function appendToDisplay(value) {
-  displayValue += value;
-  document.getElementById('display').value = displayValue;
-}
-
-// clear the display
-function clearDisplay() {
-  displayValue = '';
-  document.getElementById('display').value = displayValue;
-}
-
-// calculate the result
-function calculateResult() {
-  try {
-    const result = eval(displayValue);
-    document.getElementById('display').value = result;
-    displayValue = result.toString();
-  } catch (error) {
-    document.getElementById('display').value = 'Error';
-    displayValue = '';
-  }
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.getElementById('display');
+    let displayValue = '';
+  
+    // append a value to the display
+    function appendToDisplay(value) {
+      displayValue += value;
+      display.value = displayValue;
+    }
+  
+    // clear the display
+    function clearDisplay() {
+      displayValue = '';
+      display.value = '';
+    }
+  
+    // calculate the result
+    function calculateResult() {
+      try {
+        const result = eval(displayValue);
+        display.value = result;
+        displayValue = result.toString();
+      } catch (error) {
+        display.value = 'Error';
+        displayValue = '';
+      }
+    }
+  
+    // Add event listeners for the buttons
+    const buttons = document.querySelectorAll('.buttons button');
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        const buttonValue = button.textContent;
+        if (buttonValue === '=') {
+          calculateResult();
+        } else if (buttonValue === 'C') {
+          clearDisplay();
+        } else {
+          appendToDisplay(buttonValue);
+        }
+      });
+    });
+  
+    // Allow keyboard input
+    document.addEventListener('keydown', function (event) {
+      if (/^[0-9+\-*/.C=]$/.test(event.key)) {
+        if (event.key === 'C') {
+          clearDisplay();
+        } else if (event.key === '=') {
+          calculateResult();
+        } else {
+          appendToDisplay(event.key);
+        }
+      }
+    });
+  });
+  
