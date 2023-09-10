a20af76fa20c787a8d5d13e15dfe478112bd90ea
Oge <52808493+ogeitunu@users.noreply.github.com>
2023-09-10 15:30:48 +0100

    ns

--- calculate.js
+++ calculate.js
@@ -1,45 +1,29 @@
-// add two numbers
-function add(a, b) {
-    return a + b;
-  }
-  
-  // subtract two numbers
-  function subtract(a, b) {
-    return a - b;
-  }
-  
-  //  multiply two numbers
-  function multiply(a, b) {
-    return a * b;
-  }
-  
-  // divide two numbers
-  function divide(a, b) {
-    if (b === 0) {
-      return "Cannot divide by zero";
-    }
-    return a / b;
-  }
-  
-  // perform calculation based on operator
-  function calculate(a, b, operator) {
-    switch (operator) {
-      case "+":
-        return add(2, 3);
-      case "-":
-        return subtract(5, 3);
-      case "*":
-        return multiply(2, 1);
-      case "/":
-        return divide(6, 2);
-      default:
-        return "Invalid operator";
-    }
-  }
-  // Example:
-  const num1 = 10;
-  const num2 = 5;
-  const operator = "+";
-  const result = calculate(num1, num2, operator);
-  console.log(`Result: ${result}`);
-  
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button =>{
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case  'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                 } catch{
+                    display.innerText= " Error";
+        }
+        break;
+        case '←':
+            if (display.innerText){
+               display.innerText = display.innerText.slice(0, -1);
+            }
+            break;
+
+        default:
+            display.innerText += e.target.innerText;
+        }
+
+    });
+});
